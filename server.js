f1f7a5a8a8033887081638b3759aad6905ee573e
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-03-04 07:38:30 -0600

    commit

--- server.js
+++ server.js
@@ -7,14 +7,25 @@ const connectDB = require("./config/db")
 const { errorHandler } = require("./middleware/errorMiddleware")
 const dotenev = require('dotenv').config();
 const port = process.env.PORT || 5000
-
+var cors = require('cors')
 connectDB()
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('tiny'))
 
-app.use('/api', require("./routes/goalRoutes"))
+var allowlist = ['http://localhost:3000', 'https://goalapp.netlify.app']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use('/api',cors(corsOptionsDelegate), require("./routes/goalRoutes"))
 
 app.use(errorHandler)
 app.listen(port, ()=>{
